a34a8ea062b733429fa9eb645374b790d0c5e5fc
DivyanshPatel <nealbay@gmail.com>
2018-01-15 16:17:47 +0530

    Input component and test for updating balance

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -2,12 +2,24 @@ import React , {Component } from 'react';
 import {connect } from 'react-redux';
 
 export class Wallet extends Component {
+  constructor(){
+    super();
+
+    this.state = {balance: undefined};
+  }
+
+  updateBalance = event => {
+    this.setState({ balance: parseInt(event.target.value,10)})
+  }
+
   render(){
     return(
       <div>
         <h3 className='balance'>
           Wallet Balance: {this.props.balance}
         </h3>
+        <br />
+        <input className='input_wallet' onChange={this.updateBalance} />
       </div>
     )
   }
--- src/components/Wallet.test.js
+++ src/components/Wallet.test.js
@@ -15,4 +15,21 @@ describe('Wallet', ()=>{
     expect(wallet.find('.balance').text()).toEqual('Wallet Balance: 20');
   });
 
-});
+  it('creates and input to deposite or withdraw from the balance', () => {
+    expect(wallet.find('.input_wallet').exists()).toBe(true);
+  });
+
+
+
+describe('when user types into wallet input ', () => {
+  const userBalance = '25';
+
+  beforeEach(() => {
+    wallet.find('.input_wallet').simulate('change', {target: {value: userBalance}});
+  });
+
+  it('updates local wallet balance into `state` and coverts it into integer', () => {
+    expect(wallet.state().balance).toEqual(parseInt(userBalance,10));
+  });
+  });
+})
